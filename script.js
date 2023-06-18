08f688f070515a13f37ed88ea11468a6420462b5
iampenkin@gmail.com <54068102+penkin-repo@users.noreply.github.com>
2023-06-19 01:47:30 +0300

    first download

--- script.js
+++ script.js
@@ -17,61 +17,117 @@ let swiper2 = new Swiper(".mySwiper2", {
   },
 });
 
-// today day
-let today = new Date();
-let day = today.getDate();
-console.log("today is ---> ", day);
+// получить дату
+function getDay(number) {
+  let date = new Date(number);
+  return date.getDate();
+}
+// получить время
+function getTime(str) {
+  const dateTimeParts = str.split(" ");
+  const timeParts = dateTimeParts[1].split(":");
+  const hours = parseInt(timeParts[0]);
+  const minutes = parseInt(timeParts[1]);
+  const seconds = parseInt(timeParts[2]);
+  return (hours + ":" + minutes); // 14:48:0
+}
+
+function startAlarms() {
+  const linksAlarm = document.querySelectorAll(".event__link");
+  linksAlarm.forEach((item)=> {
+    item.addEventListener("click", func);
+  })
+  function func(e) {
+    console.log(e.target.getAttribute("data-event"));
+    if (!e.target.classList.contains("no-active")) {
+      e.target.classList.add("no-active")
+    }
+  }
+}
+
+
+
+// запрос
+let rawGet = JSON.stringify({
+  getProgram: "getProgram",
+});
+
+
+
+function fetchToApi(raw) {
+  let myHeaders = new Headers();
+  // myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("Content-Type", "text/plain");
+  // myHeaders.append("Access-Control-Allow-Origin", "http://localhost:63342");
+  // myHeaders.append('Access-Control-Allow-Credentials', 'true');
+
+  let requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch("https://white-june.f5-portal.ru/api.php", requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      console.log(JSON.parse(result).result);
+      eventsList = JSON.parse(result).result;
+      makeEvents(eventsList, 30);
+      startAlarms();
+    })
+    .catch((error) => console.log("error", error));
+}
+
+fetchToApi(rawGet);
+
+// global vars
+let dayToday = new Date().getDate();
+let dayDisplay = 30;
+
+console.log("today is ---> ", dayToday);
 
 const sliders = document.querySelectorAll(".swiper-slide");
-sliders.forEach((item)=>{
-  item.addEventListener("click", func);
-})
+sliders.forEach((item) => {
+  item.addEventListener("click", addRemoveActiveFromOptions);
+});
 
-function func(e) {
-  e.target.classList.toggle("active")
+function addRemoveActiveFromOptions(e) {
+  e.target.classList.toggle("active");
 }
 
 const listMain = document.querySelector(".list");
 let eventsList = [];
 
-let myHeaders = new Headers();
-// myHeaders.append("Content-Type", "application/json");
-myHeaders.append("Content-Type", "text/plain");
-// myHeaders.append("Access-Control-Allow-Origin", "http://localhost:63342");
-// myHeaders.append('Access-Control-Allow-Credentials', 'true');
-
-let raw = JSON.stringify({
-  getProgram: "getProgram",
-});
-
-let requestOptions = {
-  method: "POST",
-  headers: myHeaders,
-  body: raw,
-  redirect: "follow",
-};
-
-fetch("https://white-june.f5-portal.ru/api.php", requestOptions)
-  .then((response) => response.text())
-  .then((result) => {
-    console.log(JSON.parse(result).result[0]);
-    eventsList = JSON.parse(result).result;
-    makeEvents(eventsList);
-  })
-  .catch((error) => console.log("error", error));
-
-function makeEvents(array) {
-  listMain.innerHTML = "";
+function makeEvents(array, day = 30, event = "test") {
+  listMain.innerHTML = `<h1 class="main-date container">${day} июня</h1>`;
+  let eventInner = ""
 
   array.forEach((item) => {
-    let eventInner = "";
-    item.events.forEach((item)=> {
-      eventInner += `<p>-->${item.description}</p>`
-    })
-    let li = document.createElement("li");
-    li.innerHTML = `<h2>${item.title}</h2>`;
-    li.innerHTML += `<p>${item.place}</p>`;
-    li.innerHTML += eventInner;
-    listMain.appendChild(li);
+    const dateDay = getDay(item.date);
+    if (day === dateDay) {
+      eventInner += `<h2>${item.title}</h2>`;
+
+      // события
+      item.events.forEach((item) => {
+        const timeString = `${getTime(item.date_start)}-${getTime(item.date_stop)}`
+        eventInner += `
+            <div class="list-events__place container">
+                ${item.place}
+            </div>
+            <div class="list-events__event event container">
+            <div class="event__top">
+                <div class="event__time">${timeString}</div>
+                <div class="event__link" data-event="${item.id}">Напомнить мне</div>
+            </div>
+            <div class="event__body">
+                ${item.description}
+            </div>
+            </div>
+        `;
+      });
+
+      listMain.innerHTML += eventInner;
+    }
   });
 }
